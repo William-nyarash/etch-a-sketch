8913d993f66a82934f93e1e0ccc45d2d770ffc74
William-nyarash <owetywilliamnyaranga@gmail.com>
2025-08-04 17:26:28 +0300

    add a random color generator

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ const body = document.querySelector(".body")
 const button = document.createElement("div")
 
 button.classList.add("button")
-button.textContent = "click me"
+button.textContent = "start"
 body.appendChild(button)
-
+const randomValue = () => {
+	const value = Math.floor(Math.random() * 256)
+	return value
+}
 button.addEventListener("click", () => {
 	
 	const numberOfSquares =	parseInt(prompt("Enter the number of squares you want generated "))
@@ -18,15 +21,15 @@ button.addEventListener("click", () => {
 		}
 
 	body.appendChild(button)
-
 	for(let i = 0; i < numberOfSquares; i ++){
 	for (let n = 0; n < numberOfSquares; n ++) {
 		const div = document.createElement("div")
 		div.classList.add('div')
 		div.addEventListener("mouseover", ()=> {
-			div.classList.add("hover_div");
+			div.style.backgroundColor =	`rgb(${randomValue()}, ${randomValue()} , ${randomValue()})`
 		})
 		container.appendChild(div)
 }
 }
 })
+
