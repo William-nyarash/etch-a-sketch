100cb5d04b148f0b958ce2c5a5976dfa4ae51af2
William-nyarash <owetywilliamnyaranga@gmail.com>
2025-08-04 15:53:35 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const container = document.querySelector(".container");
+
+let number = 16;
+	for(let i = 0; i < number; i ++){
+		for (let n = 0; n < number; n ++) {
+			const div = document.createElement("div")
+			div.classList.add('div')
+		container.appendChild(div)
+	}
+}
+
